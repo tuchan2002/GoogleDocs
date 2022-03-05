e30593a2e393aff5061eada5c841d55c8442a0b2
TranAnhTu17 <20020495@vnu.edu.vn>
2022-03-05 20:13:44 +0700

    fix handle scroll window

--- js/app.js
+++ js/app.js
@@ -1,63 +1,63 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+window.onload = function () {
+    const $ = document.querySelector.bind(document);
+    const $$ = document.querySelectorAll.bind(document);
 
-const sectionHeader = $(".header");
-const sectionCreate = $(".create");
-const documentContainerTop = $(".document-container-top");
-const documentMain = $(".document-main");
-const addBtn = $(".add-btn");
-const editLogo = $(".edit-logo");
-const editSection = $(".edit");
-const createItemBlank = $(".create-item-image");
+    const sectionHeader = $(".header");
+    const sectionCreate = $(".create");
+    const documentContainerTop = $(".document-container-top");
+    const documentMain = $(".document-main");
+    const addBtn = $(".add-btn");
+    const editLogo = $(".edit-logo");
+    const editSection = $(".edit");
+    const createItemBlank = $(".create-item-image");
 
-const sectionCreateHeight = sectionCreate.offsetHeight;
-const documentContainerTopHeight = documentContainerTop.offsetHeight;
+    const sectionCreateHeight = sectionCreate.offsetHeight;
+    const documentContainerTopHeight = documentContainerTop.offsetHeight;
 
-const app = (function () {
-    return {
-        handleEvents() {
-            // window scroll
-            const handleScroll = () => {
-                if (window.scrollY >= sectionCreateHeight) {
-                    addBtn.classList.add("active");
-                    documentContainerTop.classList.add("active");
-                    documentMain.style = `margin-top: ${documentContainerTopHeight}px`;
-                } else {
-                    addBtn.classList.remove("active");
-                    documentContainerTop.classList.remove("active");
-                    documentMain.style = `margin: 0`;
-                }
-            };
-            window.addEventListener("scroll", handleScroll);
+    const app = (function () {
+        return {
+            handleEvents() {
+                // window scroll
+                const handleScroll = () => {
+                    if (window.scrollY >= sectionCreateHeight) {
+                        addBtn.classList.add("active");
+                        documentContainerTop.classList.add("active");
+                        documentMain.style = `margin-top: ${documentContainerTopHeight}px`;
+                    } else {
+                        addBtn.classList.remove("active");
+                        documentContainerTop.classList.remove("active");
+                        documentMain.style = `margin: 0`;
+                    }
+                };
+                window.addEventListener("scroll", handleScroll);
 
-            // handle open/close editSection
-            const handleOpenEditSection = () => {
-                editSection.classList.add("active");
-                const body = document.body;
-                body.style.height = '100vh';
-                body.style.overflowY = 'hidden';
-            };
-            const handleCloseEditSection = () => {
-                editSection.classList.remove("active");
-                const body = document.body;
-                body.style.height = '';
-                body.style.overflowY = '';
-            };
-            editLogo.onclick = () => {
-                handleCloseEditSection();
-            };
-            createItemBlank.onclick = () => {
-                handleOpenEditSection();
-            };
-            addBtn.onclick = () => {
-                handleOpenEditSection();
-            };
-
-
-        },
-        init() {
-            this.handleEvents();
-        },
-    };
-})();
-app.init();
+                // handle open/close editSection
+                const handleOpenEditSection = () => {
+                    editSection.classList.add("active");
+                    const body = document.body;
+                    body.style.height = "100vh";
+                    body.style.overflowY = "hidden";
+                };
+                const handleCloseEditSection = () => {
+                    editSection.classList.remove("active");
+                    const body = document.body;
+                    body.style.height = "";
+                    body.style.overflowY = "";
+                };
+                editLogo.onclick = () => {
+                    handleCloseEditSection();
+                };
+                createItemBlank.onclick = () => {
+                    handleOpenEditSection();
+                };
+                addBtn.onclick = () => {
+                    handleOpenEditSection();
+                };
+            },
+            init() {
+                this.handleEvents();
+            },
+        };
+    })();
+    app.init();
+};
